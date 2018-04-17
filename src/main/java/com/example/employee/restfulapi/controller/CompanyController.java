241e1602721b4b5d0135b4bb234e790d3ca339e4
llr392425595 <392425595@qq.com>
2018-04-17 17:15:37 +0800

    获取company列表

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -1,5 +1,28 @@
 package com.example.employee.restfulapi.controller;
 
+import com.example.employee.restfulapi.entity.Company;
+import com.example.employee.restfulapi.repository.CompanyRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/companies")
 public class CompanyController {
-    //在此处完成Company API
+
+  @Autowired
+  private CompanyRepository companyRepository;
+
+  @GetMapping("")
+  public ResponseEntity<List> getAllCompanies() {
+    List<Company> companies = companyRepository.findAll();
+    return new ResponseEntity<>(companies, HttpStatus.OK);
+  }
+
+
+
 }
