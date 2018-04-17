3950b7ddd0137687f2d4f62804e3190e76cfe204
llr392425595 <392425595@qq.com>
2018-04-17 18:12:33 +0800

    完成Company API

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -3,13 +3,15 @@ package com.example.employee.restfulapi.controller;
 import com.example.employee.restfulapi.entity.Company;
 import com.example.employee.restfulapi.entity.Employee;
 import com.example.employee.restfulapi.repository.CompanyRepository;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,19 +33,29 @@ public class CompanyController {
     return new ResponseEntity<>(companies, HttpStatus.OK);
   }
 
-  @GetMapping(value = "{/id}")
-  public ResponseEntity getCompanyById(@PathVariable("id") Long id) throws Exception {
+  @GetMapping("/{id}")
+  public ResponseEntity<?> getCompanyById(@PathVariable("id") Long id) throws Exception {
     Company company = companyRepository.findOne(id);
     return new ResponseEntity<>(company, HttpStatus.OK);
   }
 
-  @PostMapping(value = "")
+  @PostMapping("")
   public ResponseEntity addCompany(@RequestBody Company company) throws Exception {
     companyRepository.save(company);
     return new ResponseEntity<>(HttpStatus.CREATED);
   }
 
-  @PutMapping(value = "{/id}")
+  @DeleteMapping("/{id}")
+  public ResponseEntity deleteCompany(@PathVariable("id") Long id) {
+    if (companyRepository.exists(id)) {
+      companyRepository.delete(id);
+      return new ResponseEntity<>(companyRepository.findOne(id), HttpStatus.NO_CONTENT);
+    } else {
+      return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @PutMapping("/{id}")
   public ResponseEntity updateCompany(@PathVariable("id") Long id, @RequestBody Company newCompany) {
     Company company = companyRepository.findOne(id);
     company.setCompanyName(newCompany.getCompanyName());
@@ -57,5 +69,12 @@ public class CompanyController {
     Company company = companyRepository.findOne(id);
     Set<Employee> employees = company.getEmployees();
     return new ResponseEntity<>(employees, HttpStatus.OK);
+  }
+
+  @GetMapping(value = "/page/{page}/pageSize/{pageSize}")
+  public ResponseEntity getCompanyByPage(@PathVariable int page, @PathVariable int pageSize) throws Exception {
+    Pageable pageable = new PageRequest(page-1, pageSize);
+    Page companies = companyRepository.findAll(pageable);
+    return new ResponseEntity<>(companies, HttpStatus.OK);
   }
 }
